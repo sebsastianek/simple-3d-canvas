3572b02949285a657a43c56024c4175b0c45ffb9
Sebastian Krysiak <sebastian.krysiak@sbg.com.pl>
2019-11-11 16:24:09 +0100

    allow rendering behind-camera points with not fully visible polygon

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -50,16 +50,14 @@ export default class Renderer {
 
             points.forEach((point) => {
                 const product = multiplyVertexByMatrix(point, this.camera.viewMatrix.matrix);
-                if (product.z > 0) {
-                    const point2d = multiplyVertexByMatrix(product, this.camera.projectionMatrix.matrix);
-                    points2d.push(point2d);
-                    if ( point2d.x >= -1 &&  point2d.x <= 1 &&  point2d.y >= -1 &&  point2d.y <= 1) {
-                        visiblePoints.push(point2d);
-                    }
+                const point2d = multiplyVertexByMatrix(product, this.camera.projectionMatrix.matrix);
+                points2d.push(point2d);
+                if ( point2d.x >= -1 &&  point2d.x <= 1 &&  point2d.y >= -1 &&  point2d.y <= 1 && product.z > 0) {
+                    visiblePoints.push(point2d);
                 }
             });
 
-            if (visiblePoints.length === 3) {
+            if (visiblePoints.length > 0) {
                 const polygon2d = new Polygon(points2d[0], points2d[1], points2d[2]);
                 polygon2d.color = polygon.color;
                 if (!isFacedBackwards(polygon2d)) {
